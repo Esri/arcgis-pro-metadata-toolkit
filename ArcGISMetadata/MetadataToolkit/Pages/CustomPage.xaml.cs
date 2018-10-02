c4b93ce048f90fa80892298c01582298c5b0297c
Jingcheng Liu <jingcheng_liu@esri.com>
2018-10-02 05:16:16 -0700

    Step 6: add custom page

---
+++ ArcGISMetadata/MetadataToolkit/Pages/CustomPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using ArcGIS.Desktop.Metadata;
+using ArcGIS.Desktop.Metadata.Editor.Pages;
+
+namespace MetadataToolkit.Pages
+{
+  internal class CustomPageLabel : ISidebarLabel
+  {
+    string ISidebarLabel.SidebarLabel
+    {
+      get { return CustomPageLabel.SidebarLabel; }
+    }
+
+    public static string SidebarLabel
+    {
+      get { return "Custom Page"; }
+    }
+  }
+
+  /// <summary>
+  /// Interaction logic for CustomPage.xaml
+  /// </summary>
+  public partial class CustomPage : EditorPage
+  {
+    public CustomPage()
+    {
+      InitializeComponent();
+
+    }
+
+    public override string SidebarLabel
+    {
+      get { return CustomPageLabel.SidebarLabel; }
+    }
+  }
+}
