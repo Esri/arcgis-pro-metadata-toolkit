4a4d662b359a16236b9b00f504fdc7344d5a4540
Jingcheng Liu <jingcheng_liu@esri.com>
2019-01-28 07:42:42 -0800

    Fix merging issue

--- MetadataToolkitTemplate/proapp-sdk-Wizard/Defaults.cs
+++ MetadataToolkitTemplate/proapp-sdk-Wizard/Defaults.cs
@@ -11,6 +11,40 @@ See the License for the specific language governing permissions and
 limitations under the License.​
 */
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metadata_toolkit_wizard {
+
+    internal static class Defaults {
+        public static readonly string DefaultOrganization = "Acme";
+        public static readonly string[] ImageExtensions = new string[] { ".png", ".bmp", ".jpg", ".gif" };
+        public static readonly string[] controlAddinTypes = new string[] { "button", "buttonpalette", "splitbutton", 
+                              "tool","gallery", "inlinegallery", "dockpane", "pane", "menu" , 
+                              "constructiontool", "drophandler","combobox", "embeddablecontrol", "customcontrol",
+                              "propertysheet", "backstagetab"};//etc.        
+
+        public static readonly string ConfigXML = "Config.daml";
+
+
+        public static Dictionary<string, string> FileNameBuildActionMap = new Dictionary<string, string>();
+        //These are the file extensions (FileExtensions array) that this wizard will "fix". 
+        //Visual Studio changes the BuildAction for these file types since this is a WPF project type.        
+        public static readonly string[] FileExtensions = new string[] {".png", ".xaml"};
+
+        //Note: These are the rest of the extensions that Visual Studio will fix. 
+        //But these are not in a Pro Add-in so we are not "Fixing" these yet.
+        /*
+        // Treat this as "Page"
+        "XAML=Page;" +
+        // Treat these as "Resource"
+        "XML=Resource;XSL=Resource;XSLT=Resource;TXT=Resource;BMP=Resource;ICO=Resource;CUR=Resource;" +
+        "GIF=Resource;JPEG=Resource;JPE=Resource;JPG=Resource;PNG=Resource;DIB=Resource;TIFF=Resource;TIF=Resource;" +
+        "INF=Resource;COMPOSITEFONT=Resource;OTF=Resource;TTF=Resource;TTC=Resource;TTE=Resource" ;
+       */
 
         public static string Organization = "";
 
